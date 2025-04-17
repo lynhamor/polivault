9ff0d0894bc661d19cdd469911675cacc89940de
Lyndon <lyndonhamor.trycansoft@hotmail.com>
2025-04-17 22:58:39 +0800

    clean

--- src/main/java/com/speech/vault/util/JsonUItil.java
+++ src/main/java/com/speech/vault/util/JsonUItil.java
@@ -13,7 +13,6 @@ public class JsonUItil {
     public static List<String> parseJsonArrayString(String json) {
         try {
             ObjectMapper mapper = new ObjectMapper();
-            System.out.println("json: "+json);
             return mapper.readValue(json, new TypeReference<List<String>>() {});
         } catch (Exception e) {
             return Collections.emptyList();
